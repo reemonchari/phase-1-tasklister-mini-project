a9af0d8a3ef89438f45edb0b56d13ebbe6301fde
+ <+>
2024-10-14 13:21:55 +0300

    Challenge: TaskLister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form')
+  const taskInput = document.getElementById('new-task-description')
+  const taskList = document.getElementById('tasks')
+  form.addEventListener('submit', event => {
+    event.preventDefault()
+    const inputTask = taskInput.value 
+    if (inputTask === '') {
+      return 0
+    }
+    const list = document.createElement('li')
+    list.textContent = inputTask
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'x'
+    deleteButton.style.marginLeft = '10px'
+    deleteButton.addEventListener('click', () => taskList.removeChild(list))
+    
+    list.appendChild(deleteButton)
+    taskList.appendChild(list)
+    taskInput.value = ''
+  })
+
 });
